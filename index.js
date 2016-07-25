632e1710bb3fec52d4842e51de9e73a9adc150a5
Yamid Pico <ypicoleal@gmail.com>
2016-07-25 16:43:01 -0500

    correcciones

--- index.js
+++ index.js
@@ -567,7 +567,8 @@ app.post('/upload',function(req,res){
 					console.log("status response", response.statusCode);
 					console.log("response", body);
 					if (!error && response.statusCode == 200) {
-						listening.add_messages_by_type('web-empresa-' + empresa, [{motorizado: session.get_data(django_id), pedido: pedido}], function(django_id, sockets, message){
+						var json = JSON.parse(body);
+						listening.add_messages_by_type('web-empresa-' + empresa, [{motorizado: session.get_data(django_id), pedido: json, }], function(django_id, sockets, message){
 							for(var s in sockets){
 								console.log('pedido-entregado', message)
 								sockets[s].emit('pedido-entregado', message);
